c8f2ca8a3902b15708dd38fa738be85e59f86f53
Verooo <yosuagerrad@gmail.com>
2026-02-11 02:49:19 +0800

    feat: add api untuk users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,60 +16,70 @@ class UserSeeder extends Seeder
                 'role_id' => 1,
                 'email' => 'admin@example.com',
                 'password' => Hash::make('password'),
+                'phone_number' => '081234567890'
             ],
             [
                 'name' => 'Bu Dayu',
                 'role_id' => 2,
                 'email' => 'guru1@example.com',
                 'password' => Hash::make('password'),
+                'phone_number' => '081234667890'
             ],
             [
                 'name' => 'Pak Doni',
                 'role_id' => 2,
                 'email' => 'guru2@example.com',
                 'password' => Hash::make('password'),
+                'phone_number' => '081234567890'
             ],
             [
                 'name' => 'Pak Arya',
                 'role_id' => 2,
                 'email' => 'guru3@example.com',
                 'password' => Hash::make('password'),
+                'phone_number' => '081234567890',
             ],
             [
                 'name' => 'Pak Kuat',
                 'role_id' => 2,
                 'email' => 'guru4@example.com',
                 'password' => Hash::make('password'),
+                'phone_number' => '081234567890',
             ],
             [
                 'name' => 'Pak Danu',
                 'role_id' => 2,
                 'email' => 'guru5@example.com',
                 'password' => Hash::make('password'),
+                'phone_number' => '081234567890',
             ],
             [
                 'name' => 'Pak Bari',
                 'role_id' => 2,
                 'email' => 'guru6@example.com',
                 'password' => Hash::make('password'),
+                'phone_number' => '081234567890',
             ],
             [
                 'name' => 'Dapiq',
                 'role_id' => 3,
                 'email' => 'siswa1@example.com',
                 'password' => Hash::make('password'),
+                'phone_number' => '085175316050',
             ],
             [
                 'name' => 'Kanz',
                 'role_id' => 3,
                 'email' => 'siswa2@example.com',
                 'password' => Hash::make('password'),
+                'phone_number' => '081234567890',
             ],
             [
                 'name' => 'Citra',
                 'role_id' => 3,
                 'email' => 'siswa3@example.com',
                 'password' => Hash::make('password'),
+                'phone_number' => '0895327949615',
             ],
         ]);
     }
